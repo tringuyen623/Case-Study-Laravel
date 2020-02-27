75e0fb6aa2726d946811c06a8b3d1fd3b6a43b51
tringuyen623 <tringuyen2391@gmail.com>
2020-02-27 15:44:51 +0700

    add Procfile

---
+++ HotelBooking/database/seeds/HotelsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class HotelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('hotels')->insert([
+            'name' => 'Dream Hotel',
+            'address' => '28 Nguyen Tri Phuong, Tp.Hue, Viet Nam',
+            'phone' => '0987123456',
+            'email' => 'dreamhotel@demo.com',
+            'check_in_time' => '14:00',
+            'check_out_time' => '12:00',
+            'currency' => 'USD',
+            'currency_symbol' => '$'
+        ]);
+    }
+}
