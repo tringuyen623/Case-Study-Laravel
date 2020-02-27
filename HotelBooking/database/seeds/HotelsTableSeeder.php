7045155bdb7558f6a62f54097c70b6389d7b6868
tringuyen623 <tringuyen2391@gmail.com>
2020-02-27 23:27:12 +0700

    update

--- HotelBooking/database/seeds/HotelsTableSeeder.php
+++ HotelBooking/database/seeds/HotelsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class HotelsTableSeeder extends Seeder
 {
@@ -12,7 +13,13 @@ class HotelsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class)->create();
+        DB::table('users')->insert([
+            'name' => 'Admin',
+        'email' => 'admin@gmail.com',
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+        ]);
 
         DB::table('hotels')->insert([
             'name' => 'Dream Hotel',
