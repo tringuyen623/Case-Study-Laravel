c87891127079a67c2df28478243fdf4c7f73c226
tringuyen623 <tringuyen2391@gmail.com>
2020-03-05 08:44:13 +0700

    update

--- HotelBooking/database/seeds/RoomTableSeeder.php
+++ HotelBooking/database/seeds/RoomTableSeeder.php
@@ -12,55 +12,55 @@ class RoomTableSeeder extends Seeder
      */
     public function run()
     {
-        for( $i = 0; $i < 10; $i++){
+        for( $i = 0; $i < 5; $i++){
             DB::table('rooms')->insert([
                 'room_number' => 10 . $i,
                 'view' => 'Garden',
-                'room_type_id' => random_int(1,6),
+                'room_type_id' => random_int(1,5),
                 'extra_bed' => random_int(0,1),
                 'bed_id' => random_int(1,4),
                 'created_at' => now(),
                 'updated_at' => now()
             ]);
         }
-        for( $i = 0; $i < 10; $i++){
+        for( $i = 0; $i < 5; $i++){
             DB::table('rooms')->insert([
                 'room_number' => 20 . $i,
                 'view' => 'Candle lake',
-                'room_type_id' => random_int(1,6),
+                'room_type_id' => random_int(1,5),
                 'extra_bed' => random_int(0,1),
                 'bed_id' => random_int(1,4),
                 'created_at' => now(),
                 'updated_at' => now()
             ]);
         }
-        for( $i = 0; $i < 10; $i++){
+        for( $i = 0; $i < 5; $i++){
             DB::table('rooms')->insert([
                 'room_number' => 30 . $i,
                 'view' => 'Valley poll',
-                'room_type_id' => random_int(1,6),
+                'room_type_id' => random_int(1,5),
                 'extra_bed' => random_int(0,1),
                 'bed_id' => random_int(1,4),
                 'created_at' => now(),
                 'updated_at' => now()
             ]);
         }
-        for( $i = 0; $i < 10; $i++){
+        for( $i = 0; $i < 5; $i++){
             DB::table('rooms')->insert([
                 'room_number' => 40 . $i,
                 'view' => 'City view',
-                'room_type_id' => random_int(1,6),
+                'room_type_id' => random_int(1,5),
                 'extra_bed' => random_int(0,1),
                 'bed_id' => random_int(1,4),
                 'created_at' => now(),
                 'updated_at' => now()
             ]);
         }
-        for( $i = 0; $i < 10; $i++){
+        for( $i = 0; $i < 5; $i++){
             DB::table('rooms')->insert([
                 'room_number' => 50 . $i,
                 'view' => 'River view',
-                'room_type_id' => random_int(1,6),
+                'room_type_id' => random_int(1,5),
                 'extra_bed' => random_int(0,1),
                 'bed_id' => random_int(1,4),
                 'created_at' => now(),
